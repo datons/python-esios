944c4d1a5397e07ce3601f2c05eb7eee9063a4e8
Jesús López <46565164+jsulopzs@users.noreply.github.com>
2025-03-26 20:50:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="python-esios",
-    version="0.2.6",
+    version="0.2.7",
     packages=find_packages(),
     install_requires=[
         "requests",
